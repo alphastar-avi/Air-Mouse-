356df04467c8039c06e9a7c838d270d262d30b99
Avinash <81183238+alphastar-avi@users.noreply.github.com>
2026-02-10 18:17:10 +0530

    Update airpods_motion.swift

--- airpods_motion.swift
+++ airpods_motion.swift
@@ -4,9 +4,11 @@ import CoreMotion
 let manager = CMHeadphoneMotionManager()
 
 if manager.isDeviceMotionAvailable {
+    
     manager.startDeviceMotionUpdates(to: .main) { motion, _ in
         if let motion = motion {
             print("Roll: \(motion.attitude.roll), Pitch: \(motion.attitude.pitch), Yaw: \(motion.attitude.yaw)")
+            
         }
     }
     RunLoop.main.run()
