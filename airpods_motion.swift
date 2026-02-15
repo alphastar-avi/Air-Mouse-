d4afe4c2ccfc2b127ec2d13b6e701f9d692c276d
Avinash <81183238+alphastar-avi@users.noreply.github.com>
2026-02-15 22:14:41 +0530

    Remove device motion updates from airpods_motion.swift
    Removed the device motion updates for the headphone motion manager.

--- airpods_motion.swift
+++ airpods_motion.swift
@@ -3,14 +3,7 @@ import CoreMotion
 
 let manager = CMHeadphoneMotionManager()
 
-if manager.isDeviceMotionAvailable {
-    
-    manager.startDeviceMotionUpdates(to: .main) { motion, _ in
-        if let motion = motion {
-            print("Roll: \(motion.attitude.roll), Pitch: \(motion.attitude.pitch), Yaw: \(motion.attitude.yaw)")
-            
-        }
-    }
+
     RunLoop.main.run()
 } else {
     print("No headphone motion data available.")
