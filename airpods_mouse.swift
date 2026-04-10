cf7516b5ef6c341023942ea136b36dbbee879f2d
Avinash <81183238+alphastar-avi@users.noreply.github.com>
2026-04-10 22:41:51 +0530

    Update airpods_mouse.swift

--- airpods_mouse.swift
+++ airpods_mouse.swift
@@ -14,15 +14,7 @@ if manager.isDeviceMotionAvailable {
     manager.startDeviceMotionUpdates(to: .main) { motion, _ in
         guard let motion = motion else { return }
 
-        // Get roll (left-right) and pitch (up-down)
-        let roll = motion.attitude.roll
-        let pitch = motion.attitude.pitch
 
-        // Get current cursor location
-        let loc = NSEvent.mouseLocation
-        // Calculate desired delta
-        let targetX = CGFloat(roll * factor)
-        let targetY = CGFloat(-pitch * factor * 0.7) // scale vertical less
 
         // Smooth motion
         lastX = lastX * (1 - smoothing) + targetX * smoothing
